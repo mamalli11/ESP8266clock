f8413488fc88c308e2b8a441cd19918ab3311fac
Mohammad Javad <iman7260@gmail.com>
2025-02-09 11:52:07 +0330

    Enhance AlarmManager to support alarm names and active days, improve EEPROM handling, and ensure default values are set correctly

--- src/AlarmManager/AlarmManager.cpp
+++ src/AlarmManager/AlarmManager.cpp
@@ -1,12 +1,14 @@
 #include "AlarmManager.h"
-#include "Config.h"
 
 AlarmManager::AlarmManager()
 {
-    // Initialize all alarms to default values
     for (uint8_t i = 0; i < MAX_ALARMS; i++)
     {
-        alarms[i] = {0, 0, false};
+        alarms[i].hour = 0;
+        alarms[i].minute = 0;
+        alarms[i].active = false;
+        memset(alarms[i].name, 0, sizeof(alarms[i].name)); // مقداردهی رشته
+        memset(alarms[i].days, 0, sizeof(alarms[i].days)); // مقداردهی آرایه روزها
     }
 }
 
@@ -21,34 +23,48 @@ void AlarmManager::checkAlarms(const tm *timeInfo)
     {
         if (alarms[i].active &&
             alarms[i].hour == timeInfo->tm_hour &&
-            alarms[i].minute == timeInfo->tm_min)
+            alarms[i].minute == timeInfo->tm_min &&
+            alarms[i].days[timeInfo->tm_wday] == 1) // بررسی فعال بودن در روز جاری
         {
-            triggerAlarm();
+            triggerAlarm(alarms[i]);
         }
     }
 }
 
-void AlarmManager::setAlarm(uint8_t index, uint8_t hour, uint8_t minute, bool active)
+void AlarmManager::setAlarm(uint8_t index, uint8_t hour, uint8_t minute, bool active, const char *name, const uint8_t days[7])
 {
     if (index < MAX_ALARMS)
     {
-        alarms[index] = {hour, minute, active};
-        saveAlarms(); // Save changes to EEPROM
+        alarms[index].hour = hour;
+        alarms[index].minute = minute;
+        alarms[index].active = active;
+        strncpy(alarms[index].name, name, sizeof(alarms[index].name) - 1);
+        alarms[index].name[sizeof(alarms[index].name) - 1] = '\0'; // اطمینان از پایان رشته
+        memcpy(alarms[index].days, days, sizeof(alarms[index].days));
+
+        saveAlarms();
     }
 }
 
 Alarm AlarmManager::getAlarm(uint8_t index) const
 {
+    Alarm defaultAlarm;
+    defaultAlarm.hour = 0;
+    defaultAlarm.minute = 0;
+    defaultAlarm.active = false;
+    memset(defaultAlarm.name, 0, sizeof(defaultAlarm.name));
+    memset(defaultAlarm.days, 0, sizeof(defaultAlarm.days));
+
     if (index < MAX_ALARMS)
     {
         return alarms[index];
     }
-    return {0, 0, false}; // Return a default alarm if index is out of bounds
+    return defaultAlarm;
 }
 
 void AlarmManager::saveAlarms()
 {
-    EEPROM.begin(512);
+    EEPROM.begin(EEPROM_SIZE);
     for (uint8_t i = 0; i < MAX_ALARMS; i++)
     {
         EEPROM.put(i * sizeof(Alarm), alarms[i]);
@@ -59,18 +75,38 @@ void AlarmManager::saveAlarms()
 
 void AlarmManager::loadAlarms()
 {
-    EEPROM.begin(512);
+    EEPROM.begin(EEPROM_SIZE);
     for (uint8_t i = 0; i < MAX_ALARMS; i++)
     {
-        EEPROM.get(i * sizeof(Alarm), alarms[i]);
+        Alarm alarm;
+        EEPROM.get(350 + (i * sizeof(Alarm)), alarm);
+
+        // مقداردهی پیش‌فرض برای جلوگیری از داده‌های نادرست
+        if (alarm.hour > 23)
+            alarm.hour = 0;
+        if (alarm.minute > 59)
+            alarm.minute = 0;
+        if (alarm.active > 1)
+            alarm.active = false;
+        for (uint8_t j = 0; j < 7; j++)
+        {
+            if (alarm.days[j] > 1)
+                alarm.days[j] = 0;
+        }
+        if (alarm.name[0] == 255)
+            memset(alarm.name, 0, sizeof(alarm.name));
+
+        alarms[i] = alarm;
     }
     EEPROM.end();
 }
 
-void AlarmManager::triggerAlarm()
+void AlarmManager::triggerAlarm(const Alarm &alarm)
 {
-    // Activate the buzzer
+    Serial.print("Alarm Triggered: ");
+    Serial.println(alarm.name);
+
     digitalWrite(BUZZER_PIN, HIGH);
-    delay(1000); // Buzzer on for 1 second
+    delay(1000);
     digitalWrite(BUZZER_PIN, LOW);
 }
--- src/AlarmManager/AlarmManager.h
+++ src/AlarmManager/AlarmManager.h
@@ -1,6 +1,7 @@
 #ifndef ALARMMANAGER_H
 #define ALARMMANAGER_H
 
+#include "Config.h"
 #include <Arduino.h>
 #include <EEPROM.h>
 
@@ -9,6 +10,8 @@ struct Alarm
     uint8_t hour;
     uint8_t minute;
     bool active;
+    char name[20];   // نام آلارم (اختیاری)
+    uint8_t days[7]; // روزهایی که آلارم فعال است (0: غیرفعال، 1: فعال)
 };
 
 class AlarmManager
@@ -17,16 +20,14 @@ public:
     AlarmManager();
     void begin();
     void checkAlarms(const tm *timeInfo);
-    void setAlarm(uint8_t index, uint8_t hour, uint8_t minute, bool active);
+    void setAlarm(uint8_t index, uint8_t hour, uint8_t minute, bool active, const char *name, const uint8_t days[7]);
     Alarm getAlarm(uint8_t index) const;
     void saveAlarms();
     void loadAlarms();
 
 private:
-    static const uint8_t MAX_ALARMS = 3; // Maximum number of alarms
     Alarm alarms[MAX_ALARMS];
-
-    void triggerAlarm();
+    void triggerAlarm(const Alarm &alarm);
 };
 
 #endif
