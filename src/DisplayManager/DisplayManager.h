99cd7f38c39bbb19924a522ffa34f5e050ab31ad
Mohammad Javad <iman7260@gmail.com>
2025-03-17 09:13:56 +0330

    Refactor DisplayManager to accept WeatherManager object for weather display, improving data handling and reducing parameter complexity

--- src/DisplayManager/DisplayManager.h
+++ src/DisplayManager/DisplayManager.h
@@ -3,6 +3,7 @@
 
 #include <U8g2lib.h>
 #include <time.h>
+#include "../WeatherManager/WeatherManager.h"
 
 class DisplayManager
 {
@@ -10,21 +11,19 @@ public:
     DisplayManager();
     void initialize();
     void showClockPage(const tm *timeInfo, const String &shamsiDate);
-    void showWeatherPage(const String &description, float temp, float humidity);
+    void showWeatherPage(const WeatherManager &weather);
     void showPrayerTimesPage(const String times[6]);
     void showMessage(const String &message, int duration = 2000);
     void showErrorMessage(const String &error);
     void clearDisplay();
-
     void adjustBrightness(int sensorValue);
-    void drawWeatherIcon(const String &icon);
 
 private:
     U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2;
 
     void drawTime(const tm *timeInfo, bool is24Hour);
     void drawDate(const String &date);
-    void drawWeather(const String &description, float temp, float humidity);
+    void drawWeather(const WeatherManager &weather);
     void drawPrayerTimes(const String times[6]);
 };
 
