636e6ef451eaaae0507b8ccf7c1232d2c641bbf5
Mohammad Javad <iman7260@gmail.com>
2025-03-17 09:14:14 +0330

    Add header file for weather bitmaps to WeatherManager

---
+++ src/WeatherManager/bitmaps.h
@@ -0,0 +1,20 @@
+#ifndef BITMAPS_H
+#define BITMAPS_H
+
+#include <stdint.h>
+
+extern const uint8_t sun_bitmap[];
+extern const uint8_t moon_bitmap[];
+extern const uint8_t cloud_sun_bitmap[];
+extern const uint8_t cloud_moon_bitmap[];
+extern const uint8_t cloud_bitmap[];
+extern const uint8_t cloud_dense_bitmap[];
+extern const uint8_t rain_bitmap[];
+extern const uint8_t drizzle_bitmap[];
+extern const uint8_t drizzle_night_bitmap[];
+extern const uint8_t storm_bitmap[];
+extern const uint8_t snow_bitmap[];
+extern const uint8_t fog_bitmap[];
+extern const uint8_t unknown_bitmap[];
+
+#endif
