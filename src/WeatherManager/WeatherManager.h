b2defac144f4ea1468ba873d388363668c4c18f9
Mohammad Javad <iman7260@gmail.com>
2025-03-17 09:14:34 +0330

    Refactor WeatherManager to support default city and country values, add wind speed retrieval, and implement EEPROM reading for location data.

--- src/WeatherManager/WeatherManager.h
+++ src/WeatherManager/WeatherManager.h
@@ -1,21 +1,26 @@
 #ifndef WEATHERMANAGER_H
 #define WEATHERMANAGER_H
 
-#include <Arduino.h>
 #include <ArduinoJson.h>
+#include <EEPROM.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClientSecure.h>
 
 class WeatherManager
 {
 public:
-    WeatherManager(const String &city, const String &country);
+    WeatherManager(const String &city = "", const String &country = "");
     void begin();
     bool update();
+    void refreshLocation();
+
+    // Getters
     String getDescription() const;
     float getTemperature() const;
     float getHumidity() const;
+    float getWindSpeed() const;
     String getWeatherIcon() const;
+    const uint8_t *getWeatherBitmap() const;
 
 private:
     String city;
@@ -23,13 +28,18 @@ private:
     String description;
     float temperature;
     float humidity;
+    float windSpeed;
     String weatherIcon;
+    const uint8_t *weatherBitmap;
+
+    String readCityFromEEPROM();
+    String readCountryFromEEPROM();
+    bool parseWeatherData(const String &jsonResponse);
+    const uint8_t *mapIconToBitmap(const char *icon);
 
     static const String apiUrl;
-    static const String apiKey;
-
-    bool parseWeatherData(const String &jsonResponse);
-    String mapWeatherToIcon(const String &weatherDescription) const;
+    static constexpr int EEPROM_CITY_ADDR = 100;
+    static constexpr int EEPROM_COUNTRY_ADDR = 150;
 };
 
 #endif
