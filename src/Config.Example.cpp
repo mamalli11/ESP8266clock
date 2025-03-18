a06f6dccea02c484246225f96c36509328ec053a
Mohammad Javad <iman7260@gmail.com>
2025-03-18 07:52:34 +0330

    Add default configuration values for WiFi SSID, password, and API keys

---
+++ src/Config.Example.cpp
@@ -0,0 +1,10 @@
+#include "Config.h"
+
+const char *DEFAULT_SSID = "Wifi-SSID";
+const char *DEFAULT_PASSWORD = "Wifi-Password";
+
+const char *WEATHER_API_KEY = "Waether-API-Key";
+
+// Before using, please find your city or province code through the description of this web service and add it to the end of the URL.
+// https://prayer.aviny.com/PrayerTimes-WebService.aspx
+const char *PRAYER_TIMES_API_URL = "https://prayer.aviny.com/api/prayertimes/***";
