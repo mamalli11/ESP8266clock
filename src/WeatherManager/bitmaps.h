4929d98b359bd0c7bddf3b28ab41a47f3b02e75f
Mohammad Javad <iman7260@gmail.com>
2025-03-25 07:10:49 +0330

    Update bitmap definitions in WeatherManager for improved weather icon representation

--- src/WeatherManager/bitmaps.h
+++ src/WeatherManager/bitmaps.h
@@ -7,14 +7,20 @@ extern const uint8_t sun_bitmap[];
 extern const uint8_t moon_bitmap[];
 extern const uint8_t cloud_sun_bitmap[];
 extern const uint8_t cloud_moon_bitmap[];
-extern const uint8_t cloud_bitmap[];
-extern const uint8_t cloud_dense_bitmap[];
-extern const uint8_t rain_bitmap[];
-extern const uint8_t drizzle_bitmap[];
-extern const uint8_t drizzle_night_bitmap[];
-extern const uint8_t storm_bitmap[];
-extern const uint8_t snow_bitmap[];
-extern const uint8_t fog_bitmap[];
+extern const uint8_t cloud_d_bitmap[];
+extern const uint8_t cloud_n_bitmap[];
+extern const uint8_t cloud_dense_d_bitmap[];
+extern const uint8_t cloud_dense_n_bitmap[];
+extern const uint8_t rain_d_bitmap[];
+extern const uint8_t rain_n_bitmap[];
+extern const uint8_t drizzle_d_bitmap[];
+extern const uint8_t drizzle_n_bitmap[];
+extern const uint8_t thunderstorm_d_bitmap[];
+extern const uint8_t thunderstorm_n_bitmap[];
+extern const uint8_t snow_d_bitmap[];
+extern const uint8_t snow_n_bitmap[];
+extern const uint8_t fog_d_bitmap[];
+extern const uint8_t fog_n_bitmap[];
 extern const uint8_t unknown_bitmap[];
 
 #endif
