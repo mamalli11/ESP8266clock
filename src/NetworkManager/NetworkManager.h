40c18dc5bac7e9834e6ebdfadaf7ba35202c9fa3
Mohammad Javad <iman7260@gmail.com>
2025-04-24 00:37:17 +0330

    Add WiFi connection check method and enhance connection tracking variables

--- src/NetworkManager/NetworkManager.h
+++ src/NetworkManager/NetworkManager.h
@@ -1,3 +1,4 @@
+// NetworkManager.h
 #ifndef NETWORKMANAGER_H
 #define NETWORKMANAGER_H
 
@@ -15,6 +16,7 @@ public:
     void handleClient();
     bool isConnected();
     String getLocalIP();
+    void checkWiFiConnection();
     void saveWiFiCredentials(const String &ssid, const String &password);
     String getSSID() const { return wifiSSID; }
     String getPassword() const { return wifiPassword; }
@@ -26,11 +28,16 @@ private:
     String wifiPassword;
     bool isAPMode;
 
+    bool wasEverConnected; // Flag to track if we ever had a successful connection
+    unsigned long lastConnectionAttempt;
+    unsigned long nextRetryDelay;
+    const unsigned long retryIntervals[8] = {30000, 60000, 120000, 300000, 600000, 1200000, 2400000, 17280000};
+    int currentRetryStep;
+
     void loadWiFiCredentials();
     void saveWiFiCredentials();
     void startAPMode();
     void connectToWiFi();
-    void checkWiFiConnection();
     void initWebServer();
     void serveFile(const String &path, const String &contentType);
 };
